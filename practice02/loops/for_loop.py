5c9e2edad204ed4758747cd3f4e4f578207be258
ShyngysZh <szhanatuly@bk.ru>
2026-01-26 19:27:03 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ practice02/loops/for_loop.py
@@ -0,0 +1,23 @@
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+  
+for x in range(2, 30, 3):
+  print(x)
+  
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  
+  
+for x in "banana":
+  print(x)
+  
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
